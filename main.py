451c97f6698b8eae0e152cd94d64aeb627013342
laissivedovato <laissi.vedovato@gmail.com>
2021-08-16 17:32:39 -0300

    #02 - automação do envio de e-mail

--- main.py
+++ main.py
@@ -25,27 +25,52 @@ pyautogui.PAUSE = 2
 tabela = pd.read_excel(r"C:\Users\laiss\Desktop\_programacao\python\minicuroso_python\Vendas - Dezembro.xlsx")
 
 # mostra a tabela
-display(tabela)
+# display(tabela)
 
-
+# soma da coluna 'valor final' e 'quantidade' da tabela
 faturamento = tabela["Valor Final"].sum()
 quantidade = tabela["Quantidade"].sum()
-# print(faturamento, quantidade)
+print(faturamento, quantidade)
 
-#automação do envio de e-mail
-# pyautogui.hotkey('alt','tab')
-# pyautogui.hotkey('ctrl', 't')
-# link = "https://mail.google.com/"
-# pyperclip.copy(link)
-# pyautogui.hotkey('ctrl', 'v')
-# pyautogui.press('enter')
+# automação do envio de e-mail
+pyautogui.hotkey("alt","tab")
+pyautogui.hotkey("ctrl", "t")
+link = "https://mail.google.com/"
+pyperclip.copy(link)
+pyautogui.hotkey("ctrl", "v")
+pyautogui.press("enter")
+
+#posições do botão "escrever" novo email
+time.sleep(5)
+pyautogui.click(x=98, y=198)
+
+#email do destinatário
+time.sleep(5)
+pyautogui.write("laissi.vetech+teste@gmail.com")
+pyautogui.press("tab")
+pyautogui.press("tab")
+
+# campo assunto
+assunto = "Relatório de Vendas"
+pyperclip.copy(assunto)
+pyautogui.hotkey("ctrl", "v")
+pyautogui.press("tab")
+
+# corpo do email
+texto = f"""
+Prezados, bom dia
+
+O faturamento foi de R$ {faturamento:,.2f}
+A quantidade foi de {quantidade:,}
+
+Att.
+Vetech
+"""
+pyautogui.write(texto)
+
+# enviar email
+pyautogui.hotkey("ctrl", "enter")
 
 
-# PEGAR AS POSIÇÕES X e Y
-# time.sleep(5)
-# print('Posicione o MOUSE')
-# time.sleep(10)
-# x, y = pyautogui.position()
-# print ("Posicao atual do mouse:")
-# print ("x = "+str(x)+" y = "+str(y))
+
 
