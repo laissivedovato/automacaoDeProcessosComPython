d596b7115061a252ac7b49829b94362a4357c8ec
laissivedovato <laissi.vedovato@gmail.com>
2021-08-17 19:17:32 -0300

    #03 - correção de bugs e comentários adicionais

--- main.py
+++ main.py
@@ -6,26 +6,39 @@ from IPython.display import display
 
 pyautogui.PAUSE = 2
 
-# pyautogui.hotkey('alt', 'tab')
-# pyautogui.hotkey('ctrl', 't')
-# link = "https://drive.google.com/drive/u/1/folders/149xknr9JvrlEnhNWO49zPcw0PW5icxga"
-# pyperclip.copy(link)
-# pyautogui.hotkey("ctrl","v")
-# pyautogui.press("enter")
+# abra algum navegador e execute este código para iniciar o processo
 
+# altera a janela para o navegador
+pyautogui.hotkey('alt', 'tab')
 
-# time.sleep(14)
-# pyautogui.click(x=388, y=376, clicks=2) #posição da pasta no google drive
-# time.sleep(15)
-# pyautogui.click(x=388, y=376) #posição do arquivo
-# pyautogui.click(x=1157, y=182) #posição do menu para fazer o download
-# pyautogui.click(x=916, y=583) #download
+# abre uma nova aba
+pyautogui.hotkey('ctrl', 't')
 
-#'r' para que o python não leia \seguido de letra, uma caractere especial
-tabela = pd.read_excel(r"C:\Users\laiss\Desktop\_programacao\python\minicuroso_python\Vendas - Dezembro.xlsx")
+# acessa o drive para fazer o download do arquivo
+link = "https://drive.google.com/drive/u/1/folders/149xknr9JvrlEnhNWO49zPcw0PW5icxga"
+pyperclip.copy(link)
+pyautogui.hotkey("ctrl","v")
+pyautogui.press("enter")
+
+time.sleep(15)
+
+#posição da pasta no google drive
+pyautogui.click(x=388, y=376, clicks=2)
+time.sleep(15)
+
+#posição do arquivo
+pyautogui.click(x=388, y=376)
+
+#posição do menu para fazer o download
+pyautogui.click(x=1157, y=182)
+
+# clica em download
+pyautogui.click(x=916, y=583)
+
+tabela = pd.read_excel(r".\Vendas - Dezembro.xlsx")
 
 # mostra a tabela
-# display(tabela)
+display(tabela)
 
 # soma da coluna 'valor final' e 'quantidade' da tabela
 faturamento = tabela["Valor Final"].sum()
@@ -41,11 +54,11 @@ pyautogui.hotkey("ctrl", "v")
 pyautogui.press("enter")
 
 #posições do botão "escrever" novo email
-time.sleep(5)
+time.sleep(10)
 pyautogui.click(x=98, y=198)
+time.sleep(25)
 
 #email do destinatário
-time.sleep(5)
 pyautogui.write("laissi.vetech+teste@gmail.com")
 pyautogui.press("tab")
 pyautogui.press("tab")
